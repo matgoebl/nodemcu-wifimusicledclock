71837af7b84fb510ceae2ae78a0ee425242543c6
Matthias Goebl <matthias.goebl@goebl.net>
2016-04-07 09:17:07 +0200

    Added error display for clock (detect timeout/network error)

--- clock.lua
+++ clock.lua
@@ -1,5 +1,6 @@
 clock_tmr=5
 clock_int=60000
+clock_warn=4*clock_int
 
 function update_clock(t,h,m,s)
  local hp=(math.floor(h*rgb_max/12)+rgb_max/2)%rgb_max
@@ -13,13 +14,19 @@ function update_clock(t,h,m,s)
   p =  string.char(0,63,0):rep(mp)..string.char(0,0,255)..string.char(3,3,0):rep(hp-mp-1)..string.char(255,0,0)..string.char(0,64,0):rep(rgb_max-hp-1)
  end
  ws2812.writergb(rgb_pin,p)
+ status.clkup=status.uptime_ms
 end
 
 function clock(run)
  tmr.stop(clock_tmr)
  if run then
+  status.clkup=0
+  rgbset(nil,{pattern="770770770",ms=0,norepeat="1"})
   nettime(update_clock)
   tmr.alarm(clock_tmr, clock_int, 1, function()
+   if(status.clkup+clock_warn<status.uptime_ms) then
+     rgbset(nil,{pattern="700700",ms=0,norepeat="1"})
+   end
    nettime(update_clock)
   end)
  end
