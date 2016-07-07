8a3058e87707de6fb5a92aa28de67c4fb50a5549
Matthias Goebl <matthias.goebl@goebl.net>
2016-07-07 22:15:39 +0200

    Show IP address at startup

--- autostart.lua
+++ autostart.lua
@@ -13,5 +13,24 @@ dofile("mqtt.lc")
 rgbset(nil,{p="770070",ms=0})
 
 tmr.alarm(0,5000,1, function()
- modeset(0)
+ local ip = wifi.sta.getip()
+ local a,b,c=string.match(ip or "","%d+.%d+.%d+.(%d)(%d?)(%d?)")
+ local p="700700700700"
+ local n
+ n=tonumber(a)
+ if n ~= nil then
+  p="000"..string.rep("070",n)
+  n=tonumber(b)
+  if n ~= nil then
+   p=p.."000"..string.rep("770",n)
+   n=tonumber(c)
+   if n ~= nil then
+    p=p.."000"..string.rep("007",n)
+   end
+  end
+ end
+ rgbset(nil,{p=p,ms=0})
+ tmr.alarm(0,10000,1, function()
+  modeset(0)
+ end)
 end)
