8593eca0a4fdd2a50713662fb021651a128e2deb
Matthias Goebl <matthias.goebl@goebl.net>
2016-11-15 21:01:58 +0100

    Added missing file for LDR light sensor Added missing files in updater
    commit b697f820f78158cf5eb0b4c1b4a83fdde61b9f30
    Author: matgoebl <matthias.goebl@goebl.net>
    night:nodemcu-wifimusicledclock$ updater

--- updater.lua
+++ updater.lua
@@ -1,6 +1,6 @@
 update_files={'cfgedit.lua','clock.lua','compile.lua','http.lua','melody.lua','modes.lua',
- 'mqtt.lua','rgb.lua','twokeys.lua','wifiautoconnect.lua','index.html',
- 'api.js','rgb.js','autostart.lua','init.lua','update.lua'}
+ 'owtemp.lua','lumsens.lua','mqtt.lua','rgb.lua','twokeys.lua','wifiautoconnect.lua',
+ 'index.html','api.js','rgb.js','autostart.lua','init.lua','update.lua'}
 delete_files={'nettime.lc','user.lua'}
 -- normally excluded: 'config.lua','init.lua','update.lua','updater.lua'
 
