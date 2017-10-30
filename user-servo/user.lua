9875e653053dfb799864b1b0d1606238875f6bd8
Matthias Goebl <matthias.goebl@goebl.net>
2017-10-30 23:53:39 +0100

    Renamed servo example script to start with user...

--- user-servo/user.lua
+++ user-servo/user.lua
@@ -1,6 +1,6 @@
-table.insert(modes,3,"servo")
+table.insert(modes,3,"userservo")
 
 function cmd.servo(p)
- local mod=modeset("servo",true)
+ local mod=modeset("userservo",true)
  return mod.cmd(p)
 end
