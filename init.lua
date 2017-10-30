aae27c191c9adc837f5fc96d57ff70fd60f9f971
Matthias Goebl <matthias.goebl@goebl.net>
2017-10-30 23:46:37 +0100

    Added failsafe timeout in init.lua after exceptions

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
--- ======== init.lua V2.0 ========
+-- ======== init.lua V2.1 ========
 -- Failsafe Start of WiFi and combined Telnet/HTTP Service
 -- Tested on NodeMCU custom build dev on 2016-05-10 20:59 (8705b9e5)
 
-key1_pin=3  -- GPIO0 "MODE"
+key1_pin=3  -- GPIO0 "MODE", with button to GND, pull-up high to boot from SPI flash
 key1_on=gpio.LOW
-key2_pin=8  -- GPIO15 "SELECT"
+key2_pin=8  -- GPIO15 "SELECT", with button to Vss, pull-down to boot from SPI flash
 key2_on=gpio.HIGH
 gpio.mode(key1_pin,gpio.INPUT,gpio.PULLUP)
 gpio.mode(key2_pin,gpio.INPUT,gpio.PULLDOWN)
@@ -14,6 +14,8 @@ ws2812.write(string.char(128,128,0))
 
 cfg={}
 pcall(function() dofile("config.lua") end)
+
+dummy,bootreason=node.bootreason()
 
 -- Start telnet/http service on port 8266
 print("start telnet/httpd")
@@ -68,9 +70,12 @@ tmr.alarm(0, 1000, 0, function()
  end
 -- Press button 1 within 1000ms to skip autostart
   if gpio.read(key1_pin) ~= key1_on and gpio.read(key2_pin) ~= key2_on then
-   print("autostart")
-   ws2812.write(string.char(128,0,0))
-   pcall(function() dofile("autostart.lua") end)
+-- If there was an exception or a watchdog reset, delay autostart for 60 seconds
+   tmr.alarm(0, ((bootreason == 2 or bootreason == 3) and 10000) or (cfg.autostart_wait and tonumber(cfg.autostart_wait)) or 1, 0, function()
+    print("autostart")
+    ws2812.write(string.char(128,0,0))
+    pcall(function() dofile("autostart.lua") end)
+   end)
   else
 -- Hold button 1 for 6000ms total to start an remote update
    tmr.alarm(0, 5000, 0, function()
